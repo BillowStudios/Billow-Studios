cab13f434ef6bab4651732e88864955b24aba7ab
BillowStudios <BillowStudiosServices@gmail.com>
2025-03-22 15:21:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,344 @@
+const observer = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('nin0-animation');
+
+    }
+  });
+});
+
+observer.observe(document.querySelector('.nin0'));
+
+const observer1 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+       entry.target.classList.add('about-animation');
+
+    }
+  });
+});
+
+observer1.observe(document.querySelector('.about'));
+
+const observer2 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('weare-animation');
+
+    }
+  });
+});
+observer2.observe(document.querySelector('.weare'));
+ 
+const observer3 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('previous-animation');
+
+    }
+  });
+});
+observer3.observe(document.querySelector('.previous'));
+
+const observer4 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('work3-animation');
+
+    }
+  });
+});
+observer4.observe(document.querySelector('.work3'));
+
+const observer5 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('work4-animation');
+
+    }
+  });
+});
+observer5.observe(document.querySelector('.work4'));
+
+const observer6 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('work5-animation');
+
+    }
+  });
+});
+observer6.observe(document.querySelector('.work5'));
+
+const observer7 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('work6-animation');
+
+    }
+  });
+});
+observer7.observe(document.querySelector('.work6'));
+
+const observer8 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('work7-animation');
+
+    }
+  });
+});
+observer8.observe(document.querySelector('.work7'));
+
+const observer9 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('work8-animation');
+
+    }
+  });
+});
+observer9.observe(document.querySelector('.work8'));
+
+const observer10 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('work1-animation');
+
+    }
+  });
+});
+observer10.observe(document.querySelector('.work1'));
+
+const observer11 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('work2-animation');
+
+    }
+  });
+});
+observer11.observe(document.querySelector('.work2'));
+
+const observerb = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('p360-animation');
+
+    }
+  });
+});
+observerb.observe(document.querySelector('.p360'));
+
+const observerb1 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('powerful-animation');
+
+    }
+  });
+});
+observerb1.observe(document.querySelector('.powerful'));
+const observerb2 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('part1-animation');
+
+    }
+  });
+});
+observerb2.observe(document.querySelector('.part1'));
+const observerb3 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('part2-animation');
+
+    }
+  });
+});
+observerb3.observe(document.querySelector('.part2'));
+const observerb4 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('part3-animation');
+
+    }
+  });
+});
+observerb4.observe(document.querySelector('.part3'));
+
+const observerc = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('exp-animation');
+
+    }
+  });
+});
+observerc.observe(document.querySelector('.exp'));
+
+const observerc1 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('air-animation');
+
+    }
+  });
+});
+observerc1.observe(document.querySelector('.air'));
+
+const observerc2 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('part11-animation');
+
+    }
+  });
+});
+observerc2.observe(document.querySelector('.part11'));
+
+const observerc3 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('part12-animation');
+
+    }
+  });
+});
+observerc3.observe(document.querySelector('.part12'));
+ 
+const observerd = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('get-animation');
+
+    }
+  });
+});
+observerd.observe(document.querySelector('.get'));
+
+const observerd1 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('part21-animation');
+
+    }
+  });
+});
+observerd1.observe(document.querySelector('.part21'));
+const observerd2 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('cta-button11-animation');
+
+    }
+  });
+});
+observerd2.observe(document.querySelector('.cta-button11'));
+const observerd3 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+
+      entry.target.classList.add('part23-animation');
+
+    }
+  });
+});
+observerd3.observe(document.querySelector('.part23'));
